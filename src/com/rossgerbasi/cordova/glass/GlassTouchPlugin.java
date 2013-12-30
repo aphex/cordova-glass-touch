66a214199c46fdf22e3405eaf73141410c0692fb
Ross Gerbasi <rgerbasi@gmail.com>
2013-12-30 12:34:10 -0600

    Updated Touch to use Core as central dispatcher due to the inability to add multiple GenericMotionListeners to a view

--- src/com/rossgerbasi/cordova/glass/GlassTouchPlugin.java
+++ src/com/rossgerbasi/cordova/glass/GlassTouchPlugin.java
@@ -1,26 +1,30 @@
 package com.rossgerbasi.cordova.glass;
 
-// import android.util.Log;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
-import org.json.JSONArray;
-import org.json.JSONException;
 
 public class GlassTouchPlugin extends CordovaPlugin implements View.OnGenericMotionListener {
 
     @Override
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-       // Log.d("GlassTouchPlugin", "Glass Touch Initialize");
-       super.initialize(cordova, webView);
-       this.webView.setOnGenericMotionListener(this);
+        //Log.d("GlassTouchPlugin", "Glass Touch Initialize");
+        super.initialize(cordova, webView);
+
+        CordovaPlugin glassCore = this.webView.pluginManager.getPlugin("GlassCore");
+        if(glassCore != null) {
+            glassCore.onMessage("setMotionListener", this);
+        } else {
+            Log.d("GlassTouchPlugin", "Unable to find GlassCore Plugin to attach listener");
+        }
     }
 
     @Override
     public boolean onGenericMotion(View v, MotionEvent event) {
+        //Log.d("GlassTouchPlugin", "Generic Motion");
         this.webView.dispatchTouchEvent(event);
         return true;
     }
